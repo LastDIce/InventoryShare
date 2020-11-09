e41d197cb9e56c0d79c43077c774bc4e37f060aa
LastDIce <45594512+LastDIce@users.noreply.github.com>
2020-11-09 20:27:25 +0900

    Add files via upload

--- src/main/java/org/lastdice/inventoryshare/InventoryShare.java
+++ src/main/java/org/lastdice/inventoryshare/InventoryShare.java
@@ -1,16 +1,29 @@
 package org.lastdice.inventoryshare;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Collections;
 public final class InventoryShare extends JavaPlugin implements Listener {
+
+    private final ArrayList<Material> BlockList = new ArrayList<>();
+    private Material nowBlock;
+    private int slot_Num = 0;
 
     @Override
     public void onEnable() {
@@ -46,6 +59,36 @@ public final class InventoryShare extends JavaPlugin implements Listener {
 
     @EventHandler
     public void invClickEvent(InventoryClickEvent e) {
+        share_Inventory((Player) e.getWhoClicked());
+    }
+
+    @EventHandler
+    public void blockPlaceEvent(BlockPlaceEvent e) {
+        share_Inventory(e.getPlayer());
+    }
+
+    @EventHandler
+    public void blockBreakEvent(BlockBreakEvent e) {
+        share_Inventory(e.getPlayer());
+    }
+
+    @EventHandler
+    public void joinEvent(PlayerJoinEvent e) {
+        share_Inventory(e.getPlayer());
+    }
+
+    @EventHandler
+    public void itemConsumeEvent(PlayerItemConsumeEvent e) {
+        share_Inventory(e.getPlayer());
+    }
+
+    @EventHandler
+    public void interactEvent(PlayerInteractEvent e) {
+        share_Inventory(e.getPlayer());
+    }
+
+    @EventHandler
+    public void inventoryDragEvent(InventoryDragEvent e) {
         share_Inventory((Player) e.getWhoClicked());
     }
 
